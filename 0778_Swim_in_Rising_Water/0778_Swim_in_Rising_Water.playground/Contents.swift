f0576f482cf35d9dbb70ccafac473ed10389530d
yanglipeng <yanglipeng95@gmail.com>
2021-02-01 10:50:59 +0800

    no message

---
+++ 0778_Swim_in_Rising_Water/0778_Swim_in_Rising_Water.playground/Contents.swift
@@ -0,0 +1,93 @@
+import UIKit
+/*
+ * problem url: https://leetcode.com/problems/swim-in-rising-water
+ */
+
+/*
+ On an N x N grid, each square grid[i][j] represents the elevation at that point (i,j).
+
+ Now rain starts to fall. At time t, the depth of the water everywhere is t. You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distance in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+
+ You start at the top left square (0, 0). What is the least time until you can reach the bottom right square (N-1, N-1)?
+
+ Example 1:
+
+ Input: [[0,2],[1,3]]
+ Output: 3
+ Explanation:
+ At time 0, you are in grid location (0, 0).
+ You cannot go anywhere else because 4-directionally adjacent neighbors have a higher elevation than t = 0.
+
+ You cannot reach point (1, 1) until time 3.
+ When the depth of water is 3, we can swim anywhere inside the grid.
+ Example 2:
+
+ Input: [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+ Output: 16
+ Explanation:
+  0  1  2  3  4
+ 24 23 22 21  5
+ 12 13 14 15 16
+ 11 17 18 19 20
+ 10  9  8  7  6
+
+ The final route is marked in bold.
+ We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
+ Note:
+ 1. 2 <= N <= 50.
+ 2. grid[i][j] is a permutation of [0, ..., N*N - 1].
+
+ */
+
+
+
+let solution = Solution()
+var grid = [[Int]]()
+let n = 5
+
+var nums = [Int]()
+
+for i in 1..<(n * n) {
+    nums.append(i)
+}
+
+nums.shuffle()
+
+nums.insert(0, at: 0)
+//print(nums)
+
+for i in 0..<n {
+    grid.append([Int]())
+    for _ in 0..<n {
+        grid[i].append(nums.first ?? 0)
+        nums.remove(at: 0)
+    }
+}
+
+grid.forEach({print($0)})
+solution.swimInWater(grid)
+//print("result: \(result)")
+
+class Solution {
+    func swimInWater(_ grid: [[Int]]) -> Int {
+        
+        // 获取n的值 （行数和列数）
+        let n = grid.count
+        
+        let last = grid[n - 1][n - 1]
+        
+        var t = 0
+        
+        var current = grid[0][0]
+        
+        while current != last {
+
+
+        }
+        
+        
+        return t
+    }
+    
+    
+}
